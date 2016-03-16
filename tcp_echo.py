310dabb0cc87543c0ae7d1859c29daa14f998717
user <mixal4enko@gmail.com>
2016-03-17 02:53:17 +0400

    fix

--- tcp_echo.py
+++ tcp_echo.py
@@ -1,5 +1,6 @@
 import socket
 import sys
+import os
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock_addr = ('0.0.0.0', 2222)
@@ -9,10 +10,11 @@ sock.listen(10)
 
 while True:
     conn, addr = sock.accept()
-    while True:
-        data = conn.recv(1024)
-        if 'close' not in data:
-            conn.sendall(data)
-        else:
-            conn.close()
-            break
+    if not os.fork():
+        while True:
+            data = conn.recv(1024)
+            if 'close' not in data:
+                conn.sendall(data)
+            else:
+                conn.close()
+                break
