5e4e79b40f119a0922f05748f4a5d41333c3c829
user <mixal4enko@gmail.com>
2016-03-17 02:42:52 +0400

    fix

--- tcp_echo.py
+++ tcp_echo.py
@@ -5,7 +5,7 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock_addr = ('0.0.0.0', 2222)
 
 sock.bind(sock_addr)
-sock.listen(1)
+sock.listen(10)
 
 while True:
     conn, addr = sock.accept()
